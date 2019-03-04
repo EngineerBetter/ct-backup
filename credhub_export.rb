3994108ec334cc7f6f8b265148a61e92fb07926d
Irbe Krumina <irbe.krumina@engineerbetter.com>
2019-03-04 16:47:12 +0000

    exports values from credhub
    Signed-off-by: Colin Simmons <colin.simmons@engineerbetter.com>

---
+++ credhub_export.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+
+require 'openssl'
+require "base64"
+
+include Base64
+
+output_dir = ENV["OUTPUT_DIR"]
+
+if output_dir.nil?
+    `mkdir -p out`
+    output_dir = "out"
+end
+
+# encryption_key = SecureRandom.hex(32)
+
+credhub_contents = `credhub export`
+
+# create the cipher for encrypting
+cipher = OpenSSL::Cipher::AES128.new(:CBC)
+cipher.encrypt
+key = cipher.random_key
+cipher_text = cipher.update(credhub_contents) + cipher.final
+
+File.write("#{output_dir}/creds.encrypted", cipher_text)
+puts "Key: " + urlsafe_encode64(key)
